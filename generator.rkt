41a477a82dea0dfc24ddfea61aa5ca64fcea421a
hahalps <1275795245@qq.com>
2021-06-27 13:23:58 +0800

    gerenrator

---
+++ generator.rkt
@@ -0,0 +1,32 @@
+#lang plait
+
+(define (make-number n)
+  (make-generator (lambda(yield)
+                    (lambda(vd)
+                      (local [(define (number n)
+                                (begin
+                                  (yield n)
+                                  (number (+ n 1))))]
+                        (number n))))))
+
+(define (make-generator proc)
+  (local [(define yield (lambda(v) v))
+          (define go (proc (lambda(v) (yield v))))]
+    (lambda()
+      (let/cc escape
+          (begin
+            (set! yield
+                  (lambda(v)
+                    (let/cc k
+                      (begin
+                        (set! go k)
+                        (escape v)))))
+            (go (void)))))))
+
+;; test
+(define g (make-number 0))
+(g)
+(g)
+(g)
+(g)
+
